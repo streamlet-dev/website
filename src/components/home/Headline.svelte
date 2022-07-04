63b43cae98266fcbb129cfea1a19deaa0480eff6
Tim Paine <t.paine154@gmail.com>
2022-07-04 22:17:22 +0000

    Scaffold out main components of home page including main hero, features highlight, pricing highlight, and contact

---
+++ src/components/home/Headline.svelte
@@ -0,0 +1,37 @@
+<section class="mb-32 text-gray-800 text-center">
+	<div
+		class="relative overflow-hidden bg-no-repeat bg-cover"
+		style="
+	  background-position: 50%;
+	  background-image: url('img/stream.jpg');
+	  height: 500px;
+	"
+	>
+		<div
+			class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
+			style="background-color: rgba(0, 0, 0, 0.75)"
+		>
+			<div class="flex justify-center items-center h-full">
+				<div class="text-center text-white px-6 md:px-12">
+					<h1 class="text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">
+						The best offer on the market <br /><span>for your business</span>
+					</h1>
+					<a
+						class="inline-block px-7 py-3 mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+						data-mdb-ripple="true"
+						data-mdb-ripple-color="light"
+						href="#!"
+						role="button">Get started</a
+					>
+					<a
+						class="inline-block px-7 py-3 bg-transparent text-white font-medium text-sm leading-snug uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+						data-mdb-ripple="true"
+						data-mdb-ripple-color="light"
+						href="#!"
+						role="button">Learn more</a
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
