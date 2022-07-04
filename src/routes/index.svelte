49c39f59f33042684b7a1065a765d145b4fe33bc
Tim Paine <t.paine154@gmail.com>
2022-07-03 22:29:39 -0400

    add tailwind

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="text-center w-full h-screen bg-gradient-to-r from-yellow-200 via-red-300 to-pink-300">
+<h1 class="pt-10 text-7xl">About page</h1>
+<div class="font-serif pt-12 text-2xl">Ciao!</div>
+</section>
